3c790fa81df72aaa2857db912bcb3c3864eeea16
fasilminale <fasiladam1290@gmail.com>
2021-04-03 13:18:38 +0300

    Versioning untracked files

--- src/Components/Drawer/BackDrop.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import "./Backdrop.css";
-class BackDrop extends React.Component {
-  render() {
-    return <div className="backdrop" onClick={this.props.close} />;
-  }
-}
-
-export default BackDrop;
--- src/Components/Drawer/index.jsx
+++
@@ -1,9 +0,0 @@
-export { default as BackDrop } from './BackDrop';
-export { default as SlidingDrawer } from './SlidingDrawer';
-
-
-
-
-
-
-
